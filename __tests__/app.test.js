1ebf7d5edb48418731e0af8d288d9e6c3aeaf4d4
joshuatdr <joshuaroper513@gmail.com>
2024-08-23 16:59:23 +0100

    create app.test and add first test for /api/topics

---
+++ __tests__/app.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const app = require('../app');
+const db = require('../db/connection');
+const seed = require('../db/seeds/seed');
+const testData = require('../db/data/test-data');
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe('GET /api/topics', () => {
+  it('200: should respond with all topics', () => {
+    return request(app)
+      .get('/api/topics')
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics.length).toBe(3);
+        topics.forEach((topic) => {
+          expect(typeof topic.description).toBe('string');
+          expect(typeof topic.slug).toBe('string');
+        });
+      });
+  });
+});
