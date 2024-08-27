58dbb92e87a4950a6e242ce965e72d1a2d3e44c1
joshuatdr <joshuaroper513@gmail.com>
2024-08-27 11:42:27 +0100

    handle error when article not found

--- models/topics.models.js
+++ models/topics.models.js
@@ -10,6 +10,9 @@ exports.selectArticle = (article_id) => {
   return db
     .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
     .then(({ rows: [selectedArticle] }) => {
+      if (!selectedArticle) {
+        return Promise.reject({ status: 404, msg: 'Not found' });
+      }
       return selectedArticle;
     });
 };
