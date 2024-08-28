44032dd7cfe0313d119d96a59f382626aaa88716
joshuatdr <joshuaroper513@gmail.com>
2024-08-28 12:00:36 +0100

    add DELETE /api/comments/:comment_id endpoint and tests

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const {
   apiControllers: { getEndpoints },
   topicControllers: { getTopics },
   articleControllers: { getArticles, getArticle, patchArticle },
-  commentControllers: { getCommentsByArticle, postComment },
+  commentControllers: { getCommentsByArticle, postComment, deleteComment },
 } = require('./controllers');
 
 const {
@@ -29,6 +29,8 @@ app.patch('/api/articles/:article_id', patchArticle);
 app.get('/api/articles/:article_id/comments', getCommentsByArticle);
 
 app.post('/api/articles/:article_id/comments', postComment);
+
+app.delete('/api/comments/:comment_id', deleteComment);
 
 app.use(psqlErrorHandler);
 app.use(customErrorHandler);
