e548e05482afeaa89630b264933bf073d6470049
joshuatdr <joshuaroper513@gmail.com>
2024-09-16 10:22:28 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./app/routes/api-router');
+const cors = require('cors');
 
 const {
   psqlErrorHandler,
@@ -8,6 +9,7 @@ const {
   serverErrorHandler,
 } = require('./app/errors');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
 app.use(psqlErrorHandler);
